c27c9d1ec44a4f83bc6979e8b08c50a5e7a5eded
ferilee <the.real.ferilee@gmail.com>
2026-06-16 20:54:18 +0700

    remove better-sqlite3 to fix docker build

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: "./idetech.sqlite"
-  }
-});
